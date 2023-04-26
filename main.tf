4b9dcd7688562b0eb29254e15ca4a775f1750e15
osamaunifonic <57386890+osamaunifonic@users.noreply.github.com>
2023-04-26 13:30:22 +0400

    uupdate the block name

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "block {
+resource "aws_instance" "block" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
