cbdfa4c667e91e26cd49b95fa3f16f0b507a33a1
osamaunifonic <57386890+osamaunifonic@users.noreply.github.com>
2023-04-27 16:03:02 +0400

    c

--- main.tf
+++ main.tf
@@ -30,9 +30,9 @@ resource "aws_vpn_connection" "chinamoily" {
   type                = "ipsec.1"
   static_routes_only  = true
   local_ipv4_network_cidr = "223.118.36.3/32"
-  remote_ipv4_network_cidr = ["20.10.14.186/32","20.10.14.197/32"]
+  remote_ipv4_network_cidr = "20.10.14.186/24"
   tunnel1_preshared_key = "EJy6SBMevT1Yz4G4"
-  tunnel1_ike_versions = "ikev1"
+  tunnel1_ike_versions = ikev1
   tunnel1_phase1_dh_group_numbers = [2]
   tunnel1_phase1_encryption_algorithms = ["AES256"]
   tunnel1_phase1_integrity_algorithms = ["SHA2-256"]
